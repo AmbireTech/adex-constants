7b39afbe68aa36de204ec56e6b6565f5ac46c2d9
Ivo paunov <ivo.paunov@gmail.com>
2018-01-19 17:41:57 +0200

    update AdSizes and AdTypes

--- items.js
+++ items.js
@@ -29,22 +29,22 @@ const ItemTypesNames = getItemTypesNames()
 const ItemModelsByType = itemsModelsById()
 
 const AdSizes = [
-    { value: '300x250', label: '300 x 250 px' },
-    { value: '728x90', label: '728 x 90 px' },
-    { value: '160x600', label: '160 x 600 px' },
-    { value: '468x60', label: '468 x 60 px' },
-    { value: '120x600', label: '120 x 600 px' },
-    { value: '300x100', label: '300 x 100 px' },
-    { value: '88x31', label: '88 x 31 px' },
-    { value: '300x600', label: '300 x 600 px' },
-    { value: '336x280', label: '336 x 280 px' },
-    { value: '320x50', label: '320 x 50 px' },
+    { valueTxt: '300x250', label: '300 x 250 px', value: '1' },
+    { valueTxt: '728x90', label: '728 x 90 px', value: '2' },
+    { valueTxt: '160x600', label: '160 x 600 px', value: '3' },
+    { valueTxt: '468x60', label: '468 x 60 px', value: '4' },
+    { valueTxt: '120x600', label: '120 x 600 px', value: '5' },
+    { valueTxt: '300x100', label: '300 x 100 px', value: '6' },
+    { valueTxt: '88x31', label: '88 x 31 px', value: '7' },
+    { valueTxt: '300x600', label: '300 x 600 px', value: '8' },
+    { valueTxt: '336x280', label: '336 x 280 px', value: '9' },
+    { valueTxt: '320x50', label: '320 x 50 px', value: '10' },
 ]
 
 const AdTypes = [
-    { value: 'html', label: 'HTML' },
-    { value: 'android', label: 'Android' },
-    { value: 'ios', label: 'iOS' },
+    { valueTxt: 'html', label: 'HTML', value: '1' },
+    { valueTxt: 'android', label: 'Android', value: '2' },
+    { valueTxt: 'ios', label: 'iOS', value: '3' },
 ]
 
 module.exports = {
