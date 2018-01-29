77d4b23ff65f34c8539d8037e2117a373f623ce6
Ivo paunov <ivo.paunov@gmail.com>
2018-01-29 17:11:46 +0200

    bid timeouts

--- exchange.js
+++ exchange.js
@@ -1,4 +1,4 @@
-const objPropsById = require('./helper').objPropsById
+const { objPropsById, toMilliseconds } = require('./helper')
 
 const BID_STATES = {
     DoesNotExist: { id: 0, value: 'DoesNotExist', label: 'BID_STATE_DOES_NOT_EXIST' },
@@ -11,8 +11,35 @@ const BID_STATES = {
 const BidStatesValues = objPropsById(BID_STATES, 'value')
 const BidStatesLabels = objPropsById(BID_STATES, 'label')
 
+const TIMEOUTS = [
+    { value: toMilliseconds({ minutes: 30 }), label: 'BID_TIMEOUT_MINUTES', labelArgs: [30] },
+    { value: toMilliseconds({ hours: 1 }), label: 'BID_TIMEOUT_HOUR', labelArgs: [1] },
+    { value: toMilliseconds({ hours: 6 }), label: 'BID_TIMEOUT_HOURS', labelArgs: [6] },
+    { value: toMilliseconds({ hours: 12 }), label: 'BID_TIMEOUT_HOURS', labelArgs: [12] },
+    { value: toMilliseconds({ hours: 24 }), label: 'BID_TIMEOUT_HOURS', labelArgs: [24] },
+    { value: toMilliseconds({ hours: 48 }), label: 'BID_TIMEOUT_HOURS', labelArgs: [48] },
+    { value: toMilliseconds({ days: 3 }), label: 'BID_TIMEOUT_DAYS', labelArgs: [3] },
+    { value: toMilliseconds({ days: 7 }), label: 'BID_TIMEOUT_DAYS', labelArgs: [7] },
+    { value: toMilliseconds({ days: 14 }), label: 'BID_TIMEOUT_DAYS', labelArgs: [14] },
+    { value: toMilliseconds({ days: 30 }), label: 'BID_TIMEOUT_DAYS', labelArgs: [30] },
+    { value: toMilliseconds({ days: 90 }), label: 'BID_TIMEOUT_DAYS', labelArgs: [90] }
+]
+
+function tmosByValue() {
+    let byValue = TIMEOUTS.reduce((memo, tmo, index) => {
+        memo[tmo.value] = tmo
+        return memo
+    }, {})
+
+    return byValue
+}
+
+const timeoutsByValue = tmosByValue()
+
 module.exports = {
     BID_STATES: BID_STATES,
     BidStatesValues: BidStatesValues,
-    BidStatesLabels: BidStatesLabels
+    BidStatesLabels: BidStatesLabels,
+    TIMEOUTS: TIMEOUTS,
+    timeoutsByValue: timeoutsByValue
 }
--- helper.js
+++ helper.js
@@ -1,3 +1,8 @@
+const MILLISECONDS = 1000
+const SECONDS = 60
+const MINUTES = 60
+const HOURS = 24
+
 function objPropsById(obj, propName) {
     let props = {}
     Object.keys(obj).map((key) => {
@@ -8,6 +13,18 @@ function objPropsById(obj, propName) {
     return props
 }
 
+function toMilliseconds({ days = 0, hours = 0, minutes = 0, seconds = 0 }) {
+    let daysMs = days * HOURS * MINUTES * SECONDS * MILLISECONDS
+    let hoursMs = hours * MINUTES * SECONDS * MILLISECONDS
+    let minutesMs = minutes * SECONDS * MILLISECONDS
+    let secondsMS = seconds * MILLISECONDS
+
+    let totalMs = daysMs + hoursMs + minutesMs + secondsMS
+
+    return totalMs
+}
+
 module.exports = {
-    objPropsById: objPropsById
+    objPropsById: objPropsById,
+    toMilliseconds: toMilliseconds
 }
