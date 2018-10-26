b3c79660ba0633c891ef842d4dacac0d7f2495d3
Ivo paunov <ivo.paunov@gmail.com>
2018-10-26 19:13:36 +0300

    update exchange  constants

--- src/exchange.js
+++ src/exchange.js
@@ -1,21 +1,17 @@
 const { objPropsById, toSeconds, mapArrayByProp } = require('./helper')
 
-const BID_STATES = {
-    DoesNotExist: { id: 0, value: 'DoesNotExist', eventName: 'NoEvent', label: 'BID_STATE_DOES_NOT_EXIST' },
-    Accepted: { id: 1, value: 'Accepted', eventName: 'LogBidAccepted', label: 'BID_STATE_ACCEPTED' },
-    Canceled: { id: 2, value: 'Canceled', eventName: 'LogBidCanceled', label: 'BID_STATE_CANCELED' },
-    Expired: { id: 3, value: 'Expired', eventName: 'LogBidExpired', label: 'BID_STATE_EXPIRED' },
-    Completed: { id: 4, value: 'Completed', eventName: 'LogBidCompleted', label: 'BID_STATE_COMPLETED' },
-
-    // NOTE: Not smart contracts states used for pending transactions check in the dapp/node (Verified)
-    // TODO: Change from confirmed to Verified
-    ConfirmedAdv: { id: 10, value: 'ConfirmedByAdvertiser', eventName: 'LogBidConfirmed', label: 'BID_STATE_CONFIRMED_BY_ADVERTISER' },
-    ConfirmedPub: { id: 11, value: 'ConfirmedByPublisher', eventName: 'LogBidConfirmed', label: 'BID_STATE_CONFIRMED_BY_PUBLISHER' }
+const BID_STATE = {
+    Unknown: { id: 0, value: 'Unknown', eventName: 'NoEvent', label: 'BID_STATE_UNKNOWN' },
+    Active: { id: 1, value: 'Active', eventName: 'LogBidCommitment', label: 'BID_STATE_ACTIVE' },
+    Canceled: { id: 2, value: 'Canceled', eventName: 'LogBidCancel', label: 'BID_STATE_CANCEL' },
+    DeliveryTimedOut: { id: 3, value: 'DeliveryTimedOut', eventName: 'LogBidTimeout', label: 'BID_STATE_DELIVERY_TIME_OUT' },
+    DeliveryFailed: { id: 4, value: 'DeliveryFailed', eventName: 'LogBidFinalize', label: 'BID_STATE_DELIVERY_FAILED' },
+    DeliverySucceeded: { id: 5, value: 'DeliverySucceeded', eventName: 'LogBidFinalize', label: 'BID_STATE_DELIVERY_SUCCEEDED' },
 }
 
-const BidStatesValues = objPropsById(BID_STATES, 'value')
-const BidStatesLabels = objPropsById(BID_STATES, 'label')
-const BidStatesEventNames = objPropsById(BID_STATES, 'eventName')
+const BidStateValues = objPropsById(BID_STATE, 'value')
+const BidStateLabels = objPropsById(BID_STATE, 'label')
+const BidStateEventNames = objPropsById(BID_STATE, 'eventName')
 
 /*
 * TODO: test toMilliseconds or use momentjs
@@ -44,14 +40,16 @@ const TIMEOUTS = [
 
 const timeoutsByValue = mapArrayByProp(TIMEOUTS, 'value')
 
-const SIGN_TYPES = {
-    Eip: { id: 0, value: 'Eip', label: 'SIGN_TYPES_EIP' },
-    EthPersonal: { id: 1, value: 'EthPersonal', label: 'SIGN_TYPES_ETH_PERSONAL' }, // Geth mode (Ledger)
-    Trezor: { id: 2, value: 'Trezor', label: 'SIGN_TYPES_TREZOR' }
+const SIGNATURE_MODE = {
+    NO_SIG: { id: 0, value: 'NO_SIG', label: 'SIGN_TYPES_NO_SIG' },
+    EIP712: { id: 1, value: 'EIP712', label: 'SIGN_TYPES_EIP712', sigPrefix: '' },
+    GETH: { id: 2, value: 'GETH', label: 'SIGN_TYPES_GETH', sigPrefix: '\x19Ethereum Signed Message:\n32' }, // Eth personal
+    TREZOR: { id: 3, value: 'TREZOR', label: 'SIGN_TYPES_TREZOR', sigPrefix: '\x19Ethereum Signed Message:\n\x20' },
+    ADEX: { id: 4, value: 'ADEX', label: 'SIGN_TYPES_ADEX', sigPrefix: '\x19Ethereum Signed Message:\n108By signing this message, you acknowledge signing an AdEx bid with the hash:\n' },
 }
 
-const SignTypesValues = objPropsById(SIGN_TYPES, 'value')
-const SignTypesLabels = objPropsById(SIGN_TYPES, 'label')
+const SignatureModeValues = objPropsById(SIGNATURE_MODE, 'value')
+const SignatureModeLabels = objPropsById(SIGNATURE_MODE, 'label')
 
 const TX_STATUS = {
     Pending: { id: 0, value: 'Pending', label: 'TRANSACTION_STATUS_PENDING' },
@@ -63,15 +61,15 @@ const TxStatusValues = objPropsById(TX_STATUS, 'value')
 const TxStatusLabels = objPropsById(TX_STATUS, 'label')
 
 module.exports = {
-    BID_STATES: BID_STATES,
-    BidStatesValues: BidStatesValues,
-    BidStatesLabels: BidStatesLabels,
-    BidStatesEventNames: BidStatesEventNames,
+    BID_STATE: BID_STATE,
+    BidStateValues: BidStateValues,
+    BidStateLabels: BidStateLabels,
+    BidStateEventNames: BidStateEventNames,
     TIMEOUTS: TIMEOUTS,
     timeoutsByValue: timeoutsByValue,
-    SIGN_TYPES: SIGN_TYPES,
-    SignTypesValues: SignTypesValues,
-    SignTypesLabels: SignTypesLabels,
+    SIGNATURE_MODE: SIGNATURE_MODE,
+    SignatureModeValues: SignatureModeValues,
+    SignatureModeLabels: SignatureModeLabels,
     TX_STATUS: TX_STATUS,
     TxStatusValues: TxStatusValues,
     TxStatusLabels: TxStatusLabels
